ffc0edba36780da101b92aaf10c24d9f48f2d938
Gosha Gaponov <egoriks1994@gmail.com>
2022-01-28 00:04:04 +0200

    set gulp

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+let proj_folders = "";
+let src_folders = "";
+
+
+
+let path = {
+    build : {
+        html : proj_folders+"/",
+        css : proj_folders+"/css/",
+        js : proj_folders+"/js/",
+        img : proj_folders+"/img/",
+        fonts : proj_folders+"/fonts/",
+    },
+    src : {
+        html : src_folders+"/*.html",
+        css : src_folders+"/scss/style.scss",
+        js : src_folders+"/js/script.js",
+        img : src_folders+"/img/**/*.{img,png,svg,gif,ico,webp}",
+        fonts : src_folders+"/fonts/*.ttf", 
+    },
+    watch : {
+        html : src_folders+"/**/*.html",
+        css : src_folders+"/scss/**/*.scss",
+        js : src_folders+"/js/**/*.js",
+        img : src_folders+"/img/**/*.{img,png,svg,gif,ico,webp}",
+    },
+    clean : "/" + proj_folders + "/",
+}
+let {src, dest} = require('gulp'),
+    gulp = require('gulp'),
+    browsersync = require('browser-sync').create();
+
+function browserSync (params){
+    browserSync.init({
+        server : {
+            baseDir : "/" + proj_folders + "/",
+        },
+        port : 3000,
+        notify : false,
+    })
+}
+function html(){
+    return src(path.src.html)
+        .pipe(dest(path.build.html))
+        .pipe(browsersync.stream())
+}
+
+let build = gulp.series(html);
+let watch = gulp.parallel(build, browserSync);
+
+exports.html = html;
+exports.build = build;
+exports.watch = watch;
+exports.default = watch;
