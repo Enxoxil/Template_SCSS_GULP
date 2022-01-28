eb793ab2cba78f3e2fced4af889feb67c57e39c5
Gosha Gaponov <egoriks1994@gmail.com>
2022-01-28 22:31:27 +0200

    fixing gulp some bugs

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,48 @@
 let proj_folders = "dist";
 let src_folders = "#src";
 
-
-
 let path = {
-    build : {
-        html : proj_folders+"/",
-        css : proj_folders+"/css/",
-        js : proj_folders+"/js/",
-        img : proj_folders+"/img/",
-        fonts : proj_folders+"/fonts/",
+    build: {
+        html: proj_folders + "/",
+        css: proj_folders + "/css/",
+        js: proj_folders + "/js/",
+        img: proj_folders + "/img/",
+        fonts: proj_folders + "/fonts/",
     },
-    src : {
-        html : src_folders+"/*.html",
-        css : src_folders+"/scss/style.scss",
-        js : src_folders+"/js/script.js",
-        img : src_folders+"/img/**/*.{img,png,svg,gif,ico,webp}",
-        fonts : src_folders+"/fonts/*.ttf", 
+    src: {
+        html: src_folders + "/*.html",
+        css: src_folders + "/scss/style.scss",
+        js: src_folders + "/js/script.js",
+        img: src_folders + "/img/**/*.{img,png,svg,gif,ico,webp}",
+        fonts: src_folders + "/fonts/*.ttf",
     },
-    watch : {
-        html : src_folders+"/**/*.html",
-        css : src_folders+"/scss/**/*.scss",
-        js : src_folders+"/js/**/*.js",
-        img : src_folders+"/img/**/*.{img,png,svg,gif,ico,webp}",
+    watch: {
+        html: src_folders + "/**/*.html",
+        css: src_folders + "/scss/**/*.scss",
+        js: src_folders + "/js/**/*.js",
+        img: src_folders + "/img/**/*.{img,png,svg,gif,ico,webp}",
     },
-    clean : "/" + proj_folders + "/",
-}
-let {src, dest} = require('gulp'),
-    gulp = require('gulp'),
-    browsersync = require('browser-sync').create();
+    clean: "/" + proj_folders + "/",
+};
+let { src, dest } = require("gulp"),
+    gulp = require("gulp"),
+    browsersync = require("browser-sync").create(),
+    fileinclude = require("gulp-file-include");
 
-function browserSync (params){
+function browserSync(params) {
     browsersync.init({
-        server : {
-            baseDir : "/" + proj_folders + "/",
+        server: {
+            baseDir: "/" + proj_folders + "/"
         },
-        port : 3000,
-        notify : false,
-    })
+        port: 3000,
+        notify: false
+    });
 }
-function html(){
+function html() {
     return src(path.src.html)
+        .pipe(fileinclude())
         .pipe(dest(path.build.html))
-        .pipe(browsersync.stream())
+        .pipe(browsersync.stream());
 }
 
 let build = gulp.series(html);
