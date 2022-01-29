7103db79b64d52d427a9dce049983099a1c8bc8f
Gosha Gaponov <egoriks1994@gmail.com>
2022-01-29 23:51:43 +0200

    fix gulp some bugs

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,84 @@
-let proj_folders = "dist";
-let src_folders = "#src";
+let p_Fold = "dist";
+let s_Fold = "#src";
 
 let path = {
     build: {
-        html: proj_folders + "/",
-        css: proj_folders + "/css/",
-        js: proj_folders + "/js/",
-        img: proj_folders + "/img/",
-        fonts: proj_folders + "/fonts/",
+        // Пути к папкам для продакшена
+        html: p_Fold + "/",
+        css: p_Fold + "/css/",
+        js: p_Fold + "/js/",
+        img: p_Fold + "/img/",
+        fonts: p_Fold + "/fonts/",
     },
     src: {
-        html: src_folders + "/*.html",
-        css: src_folders + "/scss/style.scss",
-        js: src_folders + "/js/script.js",
-        img: src_folders + "/img/**/*.{img,png,svg,gif,ico,webp}",
-        fonts: src_folders + "/fonts/*.ttf",
+        //Пути к папкам с исходниками
+        html: [s_Fold + "/*.html", "!" + s_Fold + "/_*.html"], // Ловим все файлы с .html и исключаем все файли с _.html
+        css: s_Fold + "/scss/style.scss",
+        js: s_Fold + "/js/script.js",
+        img: s_Fold + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
+        fonts: s_Fold + "/fonts/*.ttf",
     },
     watch: {
-        html: src_folders + "/**/*.html",
-        css: src_folders + "/scss/**/*.scss",
-        js: src_folders + "/js/**/*.js",
-        img: src_folders + "/img/**/*.{img,png,svg,gif,ico,webp}",
+        //Следим за исходниками
+        html: s_Fold + "/**/*.html",
+        css: s_Fold + "/scss/**/*.scss",
+        js: s_Fold + "/js/**/*.js",
+        img: s_Fold + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
     },
-    clean: "/" + proj_folders + "/",
+    clean: "./" + p_Fold + "/", // Удаление папки проекта при запуске галпа
 };
+
 let { src, dest } = require("gulp"),
     gulp = require("gulp"),
     browsersync = require("browser-sync").create(),
-    fileinclude = require("gulp-file-include");
+    fileinclude = require("gulp-file-include"),
+    del = require("del"),
+    scss = require("gulp-sass");
 
-function browserSync(params) {
+function browserSync() { // функция запуска сервера
     browsersync.init({
         server: {
-            baseDir: "/" + proj_folders + "/"
+            baseDir: "./" + p_Fold + "/",
         },
         port: 3000,
-        notify: false
+        notify: false,
     });
 }
-function html() {
+
+
+
+function html (){ // функция 
     return src(path.src.html)
-        .pipe(fileinclude())
-        .pipe(dest(path.build.html))
-        .pipe(browsersync.stream());
+    .pipe(fileinclude())
+    .pipe(dest(path.build.html))
+    .pipe(browsersync.stream())
 }
 
-let build = gulp.series(html);
-let watch = gulp.parallel(build, browserSync);
+function css(){
+    return src(path.src.css)
+    .pipe(
+        scss({
+            outputStyle: "expanded"
+        }).on('error', scss.logError)
+    )
+    .pipe(dest(path.build.css))
+    .pipe(browsersync.stream())
+}
 
+
+
+function watchFiles(){
+    gulp.watch([path.watch.html],html); // Ловим изменения файлов на лету
+}
+
+function clean(){
+    return del(path.clean);
+}
+
+let build = gulp.series(clean, gulp.parallel(css, html)); // вначале удаляем папку dist потом пихаем туда собраный проект 
+let watch = gulp.parallel(build, watchFiles, browserSync);
+
+exports.css = css;
 exports.html = html;
 exports.build = build;
 exports.watch = watch;
