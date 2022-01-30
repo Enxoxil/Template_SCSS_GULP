e4a2a944738448314268072b9b19868a049e82c8
Gosha Gaponov <egoriks1994@gmail.com>
2022-01-30 23:57:12 +0200

    fix some buug GULP

--- gulpfile.js
+++ gulpfile.js
@@ -28,11 +28,21 @@ let path = {
     clean: "./" + p_Fold + "/", // Удаление папки проекта при запуске галпа
 };
 
-let { src, dest } = require("gulp"),
+let { src, dest } = require("gulp"), // инициализация переменных
     gulp = require("gulp"),
     browsersync = require("browser-sync").create(),
     fileinclude = require("gulp-file-include"),
-    del = require("del");
+    del = require("del"),
+    autoprefixer = require('gulp-autoprefixer'),
+    group_media = require("gulp-group-css-media-queries"),
+    clean_css = require('gulp-clean-css'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify-es').default,
+    imagemin = require('gulp-imagemin'),
+    webp = require('gulp-webp'),
+    webphtml = require('gulp-webp-html'),
+    webpcss = require('gulp-webpcss');
+
 
 const scss = require('gulp-sass')(require('sass'));
 
@@ -51,6 +61,7 @@ function browserSync() { // функция запуска сервера
 function html (){ // функция 
     return src(path.src.html)
     .pipe(fileinclude())
+    .pipe(webphtml())
     .pipe(dest(path.build.html))
     .pipe(browsersync.stream())
 }
@@ -62,23 +73,84 @@ function css(){
             outputStyle: "expanded"
         })
     )
+    .pipe(
+        group_media()
+    )
+    .pipe(
+        autoprefixer({
+            overrideBrowserslist: ["last 5 versions"], // Автопрефиксер для всех браузеров последние 5 версий
+            cascade: true // стиль написания
+        })
+    )
+    .pipe(webpcss({
+        webpClass: '.webp',
+        noWebpClass: '.no-webp'
+    }))
+    .pipe(dest(path.build.css))
+    .pipe(clean_css())
+    .pipe(
+        rename({
+            extname: ".min.css"
+        })
+    )
     .pipe(dest(path.build.css))
     .pipe(browsersync.stream())
 }
 
+function js (){ // функция 
+    return src(path.src.js)
+    .pipe(fileinclude())
+    .pipe(dest(path.build.js))
+    .pipe(
+        uglify()
+    )
+    .pipe(
+        rename({
+            extname: ".min.js"
+        })
+    )
+    .pipe(dest(path.build.js))
+    .pipe(browsersync.stream())
+}
 
+function images (){ // функция 
+    return src(path.src.img)
+    .pipe(
+        webp({
+            quality: 70
+        })
+    )
+    .pipe(dest(path.build.img))
+    .pipe(src(path.src.img))
+    .pipe(
+        imagemin({
+            progressive: true,
+            svgoPlugins: [{ removeViewBox: false }],
+            interlaced: true,
+            optimizationLevel: 3
+        })
+    )
+    .pipe(dest(path.build.img))
+    .pipe(browsersync.stream())
+}
 
 function watchFiles(){
-    gulp.watch([path.watch.html],html); // Ловим изменения файлов на лету
+    gulp.watch([path.watch.html],html); // Ловим изменения в html и запускаем ф-ю html
+    gulp.watch([path.watch.js],js); 
+    gulp.watch([path.watch.css],css); // ловим изменения в css и запускаем ф-ю css
+    gulp.watch([path.watch.img],images);
 }
 
 function clean(){
     return del(path.clean);
 }
 
-let build = gulp.series(clean, gulp.parallel(css, html)); // вначале удаляем папку dist потом пихаем туда собраный проект 
+let build = gulp.series(clean, gulp.parallel(css, html, js, images)); // вначале удаляем папку dist потом пихаем туда собраный проект 
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
+
+exports.images = images;
+exports.js = js;
 exports.css = css;
 exports.html = html;
 exports.build = build;
